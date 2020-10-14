c164f59c836bd011dc84a6c827311483ca21c169
Mitra Chandran <mitrachandran@gmail.com>
2020-10-14 11:10:43 +0800

    First commit exercise 1week 1

---
+++ Driver.java
@@ -0,0 +1,15 @@
+public class Driver {
+
+	public static void main(String [] arg){
+		//Fish is a data type
+		Fish nemo = new Fish(); //nemo is an object, Fish is a class
+
+		nemo.Swim();
+		nemo.color="red";
+		nemo.Swim();
+
+		Fish dory = new Fish();
+		dory.Swim();
+
+	}
+}
---
+++ Fish.java
@@ -0,0 +1,10 @@
+public class Fish{
+
+	public String color = "blue";
+
+	public void Swim() {
+
+		System.out.println(color + " fish swim");
+
+	}
+}
